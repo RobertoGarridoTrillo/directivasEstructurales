539d3a280d53e05fe954c7509c869eb63bc8ac51
Roberto Garrido Trillo <robegt72@gmail.com>
2023-01-07 19:34:16 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'directivasEstructurales';
+  // ngfor
+  listaEstudiantes: any[] = [
+    {nombre: 'Tomas Gonzalez', estado: 'Promocionado'},
+    {nombre: 'Lucas Perez', estado: 'Regular'},
+    {nombre: 'Juan García', estado: 'Regular'},
+  ]
 }
